43ec6ed5233f90928066a091a7e38a21d949a60b
zennoxy <notrezwez7622@proton.me>
2025-08-17 04:45:23 -0700

    Add .next/static/chunks/src_app_c2_page_tsx_9148afbf._.js 1755431126817

---
+++ .next/static/chunks/src_app_c2_page_tsx_9148afbf._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_0e82acd0._.js",
+  "static/chunks/node_modules_0dc6f24f._.js"
+],
+    source: "dynamic"
+});
